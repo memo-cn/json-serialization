3a6a5a978b229413fd6bc7d381dc046f39a48599
memo <i@memo.cool>
2024-05-18 21:07:30 +0800

    @json-serialization/binary

---
+++ packages/binary/lib/index.ts
@@ -0,0 +1,253 @@
+import type { Deserializer, Serializer } from 'json-serialization';
+
+/**
+ * 二进制序列化算法:
+ * - 字符串序列化为: s${字符串}
+ * - 其它序列化为:   b${包含字节序信息的 JSON 字符串}
+ * ----------------------------------------------------------------------------------
+ *
+ * 字节序列化算法:（uint8 字节序 → uint8 字符串）
+ *   将每一个 uint8 字节的数值作为 Unicode 的编码, 得到的字符顺次拼接起来, 称为 uint8 字符串
+ *
+ * 字节反序列化算法:（uint8 字符串 → uint8 字节序）
+ *   取回 uint8 字符串中的每一个字符的 Unicode 编码, 作为 uint8 字节的数值。
+ *
+ * 例如:
+ *
+ * UTF-8 字符串:    abc
+ * UTF-8 uint8:    97 98 99
+ *
+ * Unicode:        97 98 99
+ * Unicode 字符串:  abc
+ *
+ * UTF-8 字符串:    天a
+ * UTF-8 uint8:    229 164 169 97
+ *
+ * Unicode:        229 164 169 97
+ * Unicode 字符串:  å¤©a
+ */
+
+let File = globalThis.File;
+let Blob = globalThis.Blob;
+let Buffer = globalThis.Buffer;
+
+// 当前环境支持 Buffer
+let supportBuffer = typeof Buffer === 'function';
+// 当前环境支持 Blob
+let supportBlob = typeof Blob === 'function';
+// 当前环境支持 File
+let supportFile = typeof File === 'function';
+
+/**
+ * 阻止 Buffer 被 JSON.stringify 预处理
+ */
+if (supportBuffer) {
+    Buffer.prototype.toJSON = null;
+}
+
+// 二进制序列化器
+export const binarySerializer: Serializer = {
+    test(value: any) {
+        if (typeof value === 'string') return true;
+        if (supportFile && value instanceof File) return true;
+        if (supportBlob && value instanceof Blob) return true;
+        if (supportBuffer && value instanceof Buffer) return true;
+        if (value instanceof Uint8Array) return true;
+        if (value instanceof ArrayBuffer) return true;
+    },
+    serialize,
+};
+
+// 二进制反序列化器
+export const binaryDeserializer: Deserializer = {
+    test: (value: any) => typeof value === 'string',
+    deserialize,
+};
+
+/** ---------------------------------------------------------------------------------- */
+
+async function serialize(arg: string | File | Blob | Buffer | ArrayBuffer | Uint8Array): Promise<string> {
+    if (typeof arg === 'string') {
+        return 's' + arg;
+    }
+    let binaryJson: BinaryJson;
+    if (supportFile && arg instanceof File) {
+        binaryJson = {
+            __type: 'File',
+            __data: await blob__To__uint8String(arg),
+            type: arg.type,
+            name: arg.name,
+            lastModified: arg.lastModified,
+            webkitRelativePath: arg.webkitRelativePath,
+        };
+    } else if (supportBlob && arg instanceof Blob) {
+        binaryJson = {
+            __type: 'Blob',
+            __data: await blob__To__uint8String(arg),
+            type: arg.type,
+        };
+    } else if (supportBuffer && arg instanceof Buffer) {
+        binaryJson = {
+            __type: 'Buffer',
+            __data: buffer__To__unit8String(arg),
+        };
+    } else if (arg instanceof Uint8Array) {
+        binaryJson = {
+            __type: 'Uint8Array',
+            __data: uint8Array__To__uint8String(arg),
+        };
+    } else if (arg instanceof ArrayBuffer) {
+        binaryJson = {
+            __type: 'ArrayBuffer',
+            __data: arrayBuffer__To__uint8String(arg),
+        };
+    } else {
+        return arg as any;
+    }
+    return 'b' + JSON.stringify(binaryJson);
+}
+
+function deserialize(arg: string): any {
+    if (typeof (arg as any) !== 'string') return arg;
+    if (arg[0] === 's') return arg.slice(1);
+    if (arg[0] !== 'b') return arg;
+    let obj: BinaryJson = JSON.parse(arg.slice(1));
+
+    let bufferJson = obj.__type === 'Buffer' ? obj : null;
+    let blobJson = obj.__type === 'Blob' ? obj : null;
+    let fileJson = obj.__type === 'File' ? obj : null;
+    const uint8ArrayJson = obj.__type === 'Uint8Array' ? obj : null;
+    const arrayBufferJson = obj.__type === 'ArrayBuffer' ? obj : null;
+
+    // node 端不支持 Blob、File 时, 转换为 Buffer
+    if ((blobJson && !supportBlob) || (fileJson && !supportFile)) {
+        if (supportBuffer) {
+            bufferJson = { ...(blobJson || fileJson!), __type: 'Buffer' };
+            blobJson = fileJson = null;
+        }
+    }
+
+    // web 端不支持 Buffer 时, 转换为 Blob
+    if (bufferJson && !supportBuffer) {
+        if (supportBlob) {
+            blobJson = { ...bufferJson, __type: 'Blob', type: '' };
+            bufferJson = null;
+        }
+    }
+
+    if (bufferJson) {
+        return uint8String__To__buffer(bufferJson.__data);
+    }
+
+    if (blobJson) {
+        return uint8String__To__blob(blobJson.__data, { type: blobJson.type });
+    }
+
+    if (fileJson) {
+        const file = new File([uint8String__To__blob(fileJson.__data)], fileJson.name, {
+            type: fileJson.type,
+            lastModified: fileJson.lastModified,
+        });
+        if (typeof fileJson.webkitRelativePath === 'string') {
+            Reflect.defineProperty(file, 'webkitRelativePath', {
+                value: fileJson.webkitRelativePath,
+                configurable: true,
+                enumerable: true,
+            });
+        }
+        return file;
+    }
+
+    if (uint8ArrayJson) {
+        return uint8String__To__uint8Array(uint8ArrayJson.__data);
+    }
+
+    if (arrayBufferJson) {
+        return uint8String__To__arrayBuffer(arrayBufferJson.__data);
+    }
+}
+
+/** ---------------------------------------------------------------------------------- */
+
+type BinaryJsonBase = {
+    // uint8 字符串
+    __data: string;
+    // 数据类型
+    __type: string;
+};
+
+type BinaryJson = Uint8ArrayJson | BufferJson | BlobJson | FileJson | ArrayBufferJson;
+
+type Uint8ArrayJson = BinaryJsonBase & {
+    __type: 'Uint8Array';
+};
+
+type ArrayBufferJson = BinaryJsonBase & {
+    __type: 'ArrayBuffer';
+};
+
+type BufferJson = BinaryJsonBase & {
+    __type: 'Buffer';
+};
+
+type BlobJson = BinaryJsonBase & {
+    __type: 'Blob';
+    // mime 类型
+    type: string;
+};
+
+type FileJson = BinaryJsonBase & {
+    __type: 'File';
+    // mime 类型
+    type: string;
+    // File 使用
+    name: string;
+    // File 使用
+    lastModified: number;
+    // File 使用
+    webkitRelativePath?: string;
+};
+
+/** ---------------------------------------------------------------------------------- */
+
+function uint8Array__To__uint8String(uint8Array: Uint8Array): string {
+    return Array.prototype.map.call(uint8Array, (byte) => String.fromCharCode(byte)).join('');
+}
+
+function uint8String__To__uint8Array(uint8string: string): Uint8Array {
+    const uint8Array = new Uint8Array(uint8string.length);
+    for (let i = 0; i < uint8string.length; i++) {
+        uint8Array[i] = uint8string.charCodeAt(i);
+    }
+    return uint8Array;
+}
+
+async function blob__To__uint8String(blob: Blob): Promise<string> {
+    const arrayBuffer = await blob.arrayBuffer();
+    const uint8Array = new Uint8Array(arrayBuffer);
+    return uint8Array__To__uint8String(uint8Array);
+}
+
+function uint8String__To__blob(uint8string: string, options?: BlobPropertyBag): Blob {
+    return new Blob([uint8String__To__uint8Array(uint8string).buffer], options);
+}
+
+function buffer__To__unit8String(buffer: Buffer) {
+    return Array.prototype.map.call(buffer, (byte) => String.fromCharCode(byte)).join('');
+}
+
+function uint8String__To__buffer(uint8string: string): Buffer {
+    const buffer = Buffer.allocUnsafe(uint8string.length);
+    for (let i = 0; i < buffer.length; i++) {
+        buffer[i] = uint8string.charCodeAt(i);
+    }
+    return buffer;
+}
+
+function uint8String__To__arrayBuffer(uint8string: string): ArrayBuffer {
+    return uint8String__To__uint8Array(uint8string).buffer;
+}
+
+function arrayBuffer__To__uint8String(arrayBuffer: ArrayBuffer): string {
+    return uint8Array__To__uint8String(new Uint8Array(arrayBuffer));
+}
---
+++ packages/binary/vite.config.ts
@@ -0,0 +1,55 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { name } from './package.json';
+import { binaryDeserializer, binarySerializer } from './lib/index';
+// import { parse, stringify } from 'json-serialization';
+import { parse, stringify } from '../json/lib';
+import { readFileSync } from 'fs';
+
+export default defineConfig({
+    plugins: [
+        {
+            name: 'binary-serializer-demo',
+            apply: 'serve',
+            configureServer(viteDevServer) {
+                viteDevServer.hot.on('json', async (oldJson) => {
+                    // console.log(json);
+                    var obj = await parse(oldJson, [binaryDeserializer]);
+                    if (Object(obj) === obj) {
+                        obj.CHANGELOG = readFileSync('./CHANGELOG.md');
+                    }
+                    var newJson = await stringify(obj, [binarySerializer], 4);
+                    console.log('obj:', obj);
+                    // console.log('json:', newJson);
+
+                    viteDevServer.hot.send('json', newJson);
+                });
+            },
+        },
+        {
+            name: 'inject-title',
+            apply: 'serve',
+            transformIndexHtml(html, ctx) {
+                return {
+                    html,
+                    tags: [
+                        {
+                            tag: 'title',
+                            injectTo: 'head-prepend',
+                            children: name,
+                        },
+                    ],
+                };
+            },
+        },
+    ],
+    build: {
+        lib: {
+            entry: resolve('./lib/index.ts'),
+            formats: ['es', 'cjs'],
+            fileName(format, name) {
+                return `${name}.${format === 'es' ? 'mjs' : 'cjs'}`;
+            },
+        },
+    },
+});

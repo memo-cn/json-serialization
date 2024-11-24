9041298f866195ddcf265eeecb9ca2b71ec991ce
memo <i@memo.cool>
2024-11-24 12:41:17 +0800

    算法优化 & 补充了循环引用处理函数的一些注释

--- packages/json/lib/circular-reference.ts
+++ packages/json/lib/circular-reference.ts
@@ -1,5 +1,6 @@
 import { createPrefixEncoder } from './prefix-encoder';
 
+// 创建一个用于编码和解码循环引用路径的编码器
 const referenceEncoder = createPrefixEncoder<string[]>({
     prefix: '$ref:',
     stringify: JSON.stringify,
@@ -26,49 +27,70 @@ const referenceEncoder = createPrefixEncoder<string[]>({
  * 如果入参的某部分不包含循环引用，则返回的数据结构中相应位置会直接引用这一部分，而不是其副本。
  */
 export function replaceCircularReference<T = any>(data: T): T {
-    const valueToPathMap = new WeakMap<any, any>();
+    // 存储值到路径的映射
+    const valueToPathMap = new WeakMap<any, string[]>();
 
+    // 存储原始值到新值的映射
     const valueToNewValue = new Map<any, any>();
 
+    // 开始替换循环引用
     return replace(data, []);
 
+    // 递归替换函数
     function replace(value: any, path: string[]) {
+        // 如果值已经被处理过，直接返回缓存的新值
         if (valueToNewValue.has(value)) {
             return valueToNewValue.get(value);
         }
 
+        // 设置并缓存新值
         function setNewValue(newValue: any) {
             valueToNewValue.set(value, newValue);
             return newValue;
         }
 
+        // 如果值是字符串，进行编码处理
         if (typeof value === 'string') {
             return setNewValue(referenceEncoder.encode(value));
         }
 
+        // 如果值不是对象，直接返回
         if (Object(value) !== value) {
-            return setNewValue(value);
+            return value;
         }
 
+        // 如果值是对象, 检查是否已经存在路径
         const existingPath = valueToPathMap.get(value);
+        // 如果已经存在, 说明之前被处理过
         if (existingPath) {
+            valueToPathMap.delete(value);
+            // 直接返回首次处理时的路径，并建立值与路径字符串的映射关系。第 3 次及以后将直接从缓存中取到编码后的字符串路径
             return setNewValue(referenceEncoder.encode(existingPath));
+        } else {
+            // 将当前值和路径存储。后续该值被第 2 次处理时，直接返回路径
+            // 现在不立即, 而是等到值被第 2 次处理时再进行序列化, 是为了避免不必要的计算
+            valueToPathMap.set(value, path);
         }
 
-        valueToPathMap.set(value, path);
-
+        // 复制对象或数组
         let clone: typeof value = Array.isArray(value) ? [] : {};
+        // 是否包含循环引用
         let containsCircularReference = false;
+        // 遍历键值对
         for (let [key, originalValue] of Object.entries(value)) {
+            // 递归处理子值
             const newValue = replace(originalValue, path.concat(key));
+            // 如果 clone 的当前属性与新值不同，则更新 clone 的该属性为新值
             if (clone[key] !== newValue) {
                 clone[key] = newValue;
             }
+            // 如果原始值与新值不同，表明存在循环引用
             if (originalValue !== newValue) {
                 containsCircularReference = true;
             }
         }
 
+        // 如果包含循环引用，返回克隆的值，否则返回原值
         return containsCircularReference ? clone : value;
     }
 }
@@ -86,38 +108,56 @@ export function replaceCircularReference<T = any>(data: T): T {
  *   该函数会直接修改输入的数据结构。
  */
 export function restoreCircularReference<T = any>(data: T): T {
-    const valueToRefValue = new Map<any, any>();
 
+    // 用于存储值到引用值或缓存值的映射，用于恢复引用关系
+    const valueToNewValue = new Map<any, any>();
+
+    // 开始恢复循环引用
     return restore(data);
 
+    // 递归恢复函数
     function restore(value: any) {
-        if (Object(value) === value) {
-            if (valueToRefValue.has(value)) {
-                return valueToRefValue.get(value);
-            } else {
-                valueToRefValue.set(value, value);
-            }
-            for (let [key, originalValue] of Object.entries(value)) {
-                const newValue = restore(originalValue);
-                if (newValue !== value[key]) {
-                    value[key] = newValue;
-                }
-            }
-        } else if (typeof value === 'string') {
-            if (valueToRefValue.has(value)) {
-                return valueToRefValue.get(value);
-            }
-            const strOrPath = referenceEncoder.decode(value);
-            if (typeof strOrPath === 'string') {
-                return strOrPath;
-            }
-            const refValue = getValueByPath(strOrPath);
-            valueToRefValue.set(value, refValue);
-            return refValue;
+
+        // 恢复引用关系
+        if (valueToNewValue.has(value)) {
+            return valueToNewValue.get(value);
         }
-        return value;
+
+        // 设置并缓存新值
+        function setNewValue(newValue: any) {
+            valueToNewValue.set(value, newValue);
+            return newValue;
+        }
+
+        if (typeof value === 'string') {
+            // 解码字符串，可能得到路径或原始字符串
+            const strOrPath = referenceEncoder.decode(value);
+            // 本来就是字符串
+            if (typeof strOrPath === 'string') {
+                return setNewValue(strOrPath);
+            } else {
+                // 恢复路径获取引用值
+                const refValue = getValueByPath(strOrPath);
+                return setNewValue(refValue);
+            }
+        }
+
+        if (Object(value) !== value) {
+            return value;
+        }
+
+        for (let [key, originalValue] of Object.entries(value)) {
+            // 递归处理子值
+            const newValue = restore(originalValue);
+            if (newValue !== value[key]) {
+                value[key] = newValue;
+            }
+        }
+
+        return setNewValue(value);
     }
 
+    // 根据路径获取值
     function getValueByPath(path: string[]) {
         let current: any = data;
         for (let key of path) {
